8d3856766fdaff32fc12b3db40c030271817ea0c
bror <reachingbror@gmail.com>
2025-07-06 20:37:17 +0200

    Removed some old comments from last project.

--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -24,23 +24,3 @@ namespace EmployeesApp.Web
     }
 }
 
-// Registrera service-klassen som en transient service i Program Lägg nu till en ny anställd – varför syns den inte på index-sidan?:
-
-// builder.Services.AddTransient<EmployeeService>(); När jag registrerar min klass som transient så skapas en ny "version" av
-// EmployeeService varje gång den används. När jag sparar en Employee i min lista så sparas den i just den versionen. Laddar jag om sidan
-// får jag en ny "version" och ser inte det jag lagt til tidigare i den andra "versionen"
-
-
-
-// Registrera service-klassen som en singleton service i Program Lägg nu till en ny anställd - varför syns den nu på index-sidan?:
-
-// För att tillskillnad i från en transient klass så skickas "samma version" av min EmployeeService-klass när jag registrerat den som singleton.
-// Alltså sparas min ändringar i min "version" och samma version skickas med nästa gång jag använder klassen. 
-
-
-
-// Vad har vi vunnit på att använda Dependency Injection istället för att låta controller-klasserna själva instansiera sina beroenden?:
-
-// Det är mycket mer flexibelt. Nu skickas vår EmployeeService-klass med automatiskt och vi kan lätt byta ut den om vi vill. 
-// Nu tilldeelas våra beroende med konstruktorn och vi kan enkelt byta ut till t.ex. en testversion. 
-
